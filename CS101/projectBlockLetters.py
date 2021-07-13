6f772eb88d14d7f0e9c4a5acef4f14393252837e
firefoxSpinnie <firefoxSpinnie@protonmail.ch>
2021-07-13 17:38:20 +0200

    Add script for project Block Letters, AKA as initials.py

---
+++ CS101/projectBlockLetters.py
@@ -0,0 +1,10 @@
+# Etan Stettler
+# I'm trying to learn how to play harp, but I don't practise so much.
+
+print("E E E E E", "  S S S  ")
+print("E", "         S     S")
+print("E", "         S")
+print("E E E", "       S S")
+print("E", "               S")
+print("E", "               S")
+print("E E E E E", "  S S S")
