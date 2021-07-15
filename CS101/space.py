eb91d844e84d95dd9552f10bbd0130d9cd4b3638
firefoxSpinnie <firefoxSpinnie@protonmail.ch>
2021-07-15 20:10:17 +0200

    Update source for space.py

--- CS101/space.py
+++ CS101/space.py
@@ -1,24 +1,5 @@
-# Context : Optional miniProject at the end of the control flow lesson of the
-# second part of the career path.
-
-# Little Codey is an interplanetary space boxer, who is trying to win
-# championship belts for various weight categories on other planets within the
-# solar system.
-
-# Write a space.py program that helps him keep track of his target weight by:
-
-# Checks which number planet is equal to.
-# It should then compute his weight on the destination planet.
-
-# Here is the table of conversion:
-
-#	Planet	RelativeGravity
-# 1	Venus	0.91
-# 2	Mars	0.38
-# 3	Jupiter	2.34
-# 4	Saturn	1.06
-# 5	Uranus	0.92
-# 6	Neptune	1.19
+# https://www.codecademy.com/paths/computer-science/tracks/cspath-cs-101/
+# modules/cspath-python-control-flow/lessons/python-control-flow/exercises/review
 
 print("I have information for the following planets:\n")
 
